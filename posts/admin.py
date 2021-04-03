4cfbcf156d50d9086eefda52a08f125bbe6a6904
fuodorov <fuodorov1998@gmail.com>
2021-04-03 23:16:18 +0700

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from .models import Post
+
+
+@register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'text',
+        'pub_date',
+        'image',
+    )
+    search_fields = ('title',)
+    list_filter = ('pub_date',)
+    empty_value_display = _('-empty-')
