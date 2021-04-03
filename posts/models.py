ef01cb67f2ce6465438c225bbb46957e7d3b7773
fuodorov <fuodorov1998@gmail.com>
2021-04-03 23:10:03 +0700

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.TextField(
+        verbose_name=_('News title'),
+        help_text=_('Write the title of the news item here'),
+        max_length=32
+    )
+    text = models.TextField(
+        verbose_name=_('News text'),
+        help_text=_('Write the title of the news item here'),
+        max_length=256
+    )
+    pub_date = models.DateTimeField(
+        verbose_name=_('Date of publication'),
+        help_text=_('Date the news was published'),
+        auto_now_add=True
+    )
+    image = models.ImageField(
+        verbose_name=_('Image'),
+        help_text=_('Select a picture for the news'),
+        upload_to='posts/',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = _('News')
+        verbose_name_plural = _('News')
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.title
