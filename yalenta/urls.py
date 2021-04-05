7098cf09847644b145e8cb4a15375cc1725fb625
fuodorov <fuodorov1998@gmail.com>
2021-04-05 12:05:18 +0700

    Redoc

--- yalenta/urls.py
+++ yalenta/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls import handler404, handler500
-from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
@@ -17,6 +17,11 @@ urlpatterns = [
             {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,
             {'document_root': settings.STATIC_ROOT}),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='docs/redoc.html'),
+        name='doc'
+        ),
 ]
 
 if settings.DEBUG:
